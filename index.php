6df3b6295f826d6b542351abd8453348d47c879f
AlecTech <eremeevoleg@gmail.com>
2020-09-29 18:44:56 -0600

    initial commit test

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP page</title>
+</head>
+<body>
+    <h1>MY FIRST PAGE</h1>
+    <?php  
+        echo 'Hello, World!';
+    ?>
+
+</body>
+</html>
